285a2f4fab7dcd3e03c1145a1121c0180e8dc324
Engin Manap <enginmanap@gmail.com>
2013-04-05 21:54:42 +0300

    change color of ball

--- native-activity/jni/ball.cpp
+++ native-activity/jni/ball.cpp
@@ -46,7 +46,7 @@ private:
 		FSbasic = "precision mediump float;\n"
 		// "uniform vec4 vColor;\n"
 						"void main() {\n"
-						" gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
+						" gl_FragColor = vec4 ( 0.5, 0.5, 0.5, 1.0 );\n"
 						"}\n";
 	}
 
